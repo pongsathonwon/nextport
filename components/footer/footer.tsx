41eba319e5ef008cb639f21d628d111abd279d66
pongsathonwon <men1234-2008@hotmail.com>
2024-10-24 15:16:00 +0700

    footer

---
+++ components/footer/footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-primary flex flex-col justify-between p-4 text-neutral">
+      {/* <a className="mx-auto" href="https://spline.design/" target="_blank">
+        3D Animation by Spline
+      </a> */}
+      <p className="mx-auto">copyright 2024</p>
+    </footer>
+  );
+}
+
+export default Footer;
